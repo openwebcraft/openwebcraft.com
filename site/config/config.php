a69527651de02c1a8d68c57974c8824b90e2a46f
Matthias Geisler <matthias@openwebcraft.com>
2021-05-23 15:19:03 +0200

    remove git plugin

--- site/config/config.php
+++ site/config/config.php
@@ -21,9 +21,6 @@ return [
     'community.markdown-field.invisibles'    => false,
     'community.markdown-field.strikethrough' => true,
     'kirby-extended.highlighter.autodetect'  => true,
-    'oblik.git.repo' => '/htdocs/openwebcraft.com',
-    'oblik.git.merge' => 'kirby',
-    'oblik.git.bin' => '/bin/git',
     'matthiasjg' => [
         'static_site_composer' => [
             'endpoint' => 'compose-static-site',
