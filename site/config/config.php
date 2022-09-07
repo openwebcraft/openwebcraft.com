2d6e2b562ec4f926671931afbab4bc71e22c5217
Matthias Geisler <matthias@semanticdreamer.com>
2022-09-07 10:56:53 +0200

    enable debug

--- site/config/config.php
+++ site/config/config.php
@@ -9,7 +9,7 @@ return [
     'api' => [
         'slug' => 'kirby/api'
     ],
-    'debug' => false,
+    'debug' => true,
     'home'  => 'home',
     'community.markdown-field.font' => 'sans-serif',
     'community.markdown-field.modals'        => true,
