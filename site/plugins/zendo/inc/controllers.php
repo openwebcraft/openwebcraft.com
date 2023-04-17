d15b74a59db19f86a88b039c0d6c95770c37f708
Matthias Geisler <matthias@semanticdreamer.com>
2023-04-17 11:48:28 +0200

    fix undefined variable $seo

--- site/plugins/zendo/inc/controllers.php
+++ site/plugins/zendo/inc/controllers.php
@@ -6,10 +6,11 @@ return [
     'seo' => function ($page, $pages, $site, $kirby) {
     
         # Define the two basic seo tags    
+        $seo = Array();
         $seo['title'] = r($page->isHomePage() , $site->title()->html() . ' – ' . $site->subtitle()->html() , $page->title()->html() . ' – ' . $site->title()->html());
         $seo['desc']  = r($page->isHomePage() , $site->description() , $page->text()->excerpt(150));
 
         # Return the content
-        return compact('seo');
+        return compact($seo);
     }
 ];
