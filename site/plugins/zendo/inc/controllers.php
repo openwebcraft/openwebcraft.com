e6c649197d5cc20e4e1d86c32951b743f0877102
Matthias Geisler <matthias@semanticdreamer.com>
2023-04-17 11:53:30 +0200

    fix return tpl var

--- site/plugins/zendo/inc/controllers.php
+++ site/plugins/zendo/inc/controllers.php
@@ -4,13 +4,15 @@
 return [
 
     'seo' => function ($page, $pages, $site, $kirby) {
-    
+
         # Define the two basic seo tags    
-        $seo = Array();
-        $seo['title'] = r($page->isHomePage() , $site->title()->html() . ' – ' . $site->subtitle()->html() , $page->title()->html() . ' – ' . $site->title()->html());
-        $seo['desc']  = r($page->isHomePage() , $site->description() , $page->text()->excerpt(150));
+        $seo = array();
+        $seo['title'] = r($page->isHomePage(), $site->title()->html() . ' – ' . $site->subtitle()->html(), $page->title()->html() . ' – ' . $site->title()->html());
+        $seo['desc']  = r($page->isHomePage(), $site->description(), $page->text()->excerpt(150));
 
         # Return the content
-        return compact('seo');
+        return [
+            'seo' => $seo
+        ];
     }
 ];
